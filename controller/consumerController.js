c33ab3fed83eb7a4775df52b1c20f4e7d68603ba
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2021-12-01 14:17:07 +0530

    Pushing updated code for few implemented API's

--- controller/consumerController.js
+++ controller/consumerController.js
@@ -4,69 +4,152 @@ var amqp = require("amqplib/callback_api");
 
 exports.consumeData = async (req, res) => {
   try {
-    const { topic, id, routing_key, seconds } = req.body;
+    const { patient_id } = req.body;
+    const d = new Date();
+    const date_of_diagnosis =
+      d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
 
-    consumer(topic, routing_key, seconds, id);
+    const consumer = await pool.query(
+      "Insert into consumer(id, date_of_diagnosis) values($1,$2) RETURNING consumer_id",
+      [patient_id, date_of_diagnosis]
+    );
+    const consuming_id = consumer.rows[0].consumer_id;
 
-    res.send("Consuming the topic you have sent");
+    res.status(201).json({
+      message:
+        "You can start collecting data from each sensor now with this ID",
+      consume_id: consuming_id,
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
-function consumer(topic, routing_key, seconds, id) {
-  var data = { type: topic, user: id, vital_data: [] };
-
-  amqp.connect("amqp://localhost", function (error0, connection) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-      var exchange = topic;
-      var key = routing_key;
-
-      channel.assertExchange(exchange, "topic", {
-        durable: false,
-      });
-
-      channel.assertQueue(
-        "",
-        {
-          exclusive: true,
-        },
-        function (error2, q) {
-          if (error2) {
-            throw error2;
-          }
-          console.log(" [*] Waiting for logs. To exit press CTRL+C");
-
-          channel.bindQueue(q.queue, exchange, key);
-
-          channel.consume(
-            q.queue,
-            function (msg) {
-              // console.log(
-              //   " [x] %s:'%s'",
-              //   msg.fields.routingKey,
-              //   msg.content.toString()
-              // );
-              data.vital_data.push(msg.content.toString());
-            },
-            {
-              noAck: true,
-            }
-          );
-
-          setTimeout(function () {
-            console.log(data);
-            channel.close();
-            connection.close();
-          }, seconds * 1000);
-        }
-      );
+exports.raiseAlert = async (req, res) => {
+  try {
+    const { patient_id } = req.body;
+    res.status(201).json({
+      message: "Raised an Alert for patient",
     });
-  });
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.consumeBP = async (req, res) => {
+  try {
+    const { consume_id, seconds } = req.body;
+    var data = { type: "bp", vital_data: [] };
+
+    amqp.connect("amqp://localhost", function (error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        var exchange = "blood_pressure";
+        var key = "bp.*";
+
+        channel.assertExchange(exchange, "topic", {
+          durable: false,
+        });
+
+        channel.assertQueue(
+          "",
+          {
+            exclusive: true,
+          },
+          function (error2, q) {
+            if (error2) {
+              throw error2;
+            }
+            console.log(" [*] Waiting for logs. To exit press CTRL+C");
+
+            channel.bindQueue(q.queue, exchange, key);
+
+            channel.consume(
+              q.queue,
+              function (msg) {
+                data.vital_data.push(msg.content.toString());
+              },
+              {
+                noAck: true,
+              }
+            );
+
+            setTimeout(function () {
+              console.log(data);
+              channel.close();
+              connection.close();
+            }, seconds * 1000);
+          }
+        );
+      });
+    });
+
+    res.send(`Consuming the topic you have sent wait for ${seconds} seconds`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.consumeTemp = async (req, res) => {
+  try {
+    const { consume_id, seconds } = req.body;
+    var data = { type: "temperature", vital_data: [] };
+
+    amqp.connect("amqp://localhost", function (error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        var exchange = "temperature";
+        var key = "temp.*";
+
+        channel.assertExchange(exchange, "topic", {
+          durable: false,
+        });
+
+        channel.assertQueue(
+          "",
+          {
+            exclusive: true,
+          },
+          function (error2, q) {
+            if (error2) {
+              throw error2;
+            }
+            console.log(" [*] Waiting for logs. To exit press CTRL+C");
+
+            channel.bindQueue(q.queue, exchange, key);
+
+            channel.consume(
+              q.queue,
+              function (msg) {
+                data.vital_data.push(msg.content.toString());
+              },
+              {
+                noAck: true,
+              }
+            );
+
+            setTimeout(function () {
+              console.log(data);
+              channel.close();
+              connection.close();
+            }, seconds * 1000);
+          }
+        );
+      });
+    });
+
+    res.send(`Consuming the topic you have sent wait for ${seconds} seconds`);
+  } catch (error) {
+    console.error(error);
+  }
+};
--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -4,4 +4,10 @@ const consumerController = require("../controller/consumerController");
 
 router.post("/consume", consumerController.consumeData);
 
+router.post("/consume/bp", consumerController.consumeBP);
+
+router.post("/consume/temperature", consumerController.consumeTemp);
+
+router.post("/emergency", consumerController.raiseAlert);
+
 module.exports = router;
