d2b9ba27ffdcae3da2c49c9fa55ed60414ffd45b
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2021-12-18 22:22:31 +0530

    Calculated Normal Values for BP and Temp

--- controller/consumerController.js
+++ controller/consumerController.js
@@ -149,7 +149,12 @@ exports.consumeTemp = async (req, res) => {
             );
 
             setTimeout(function () {
-              console.log(data);
+              // console.log(data);
+              Calc_CriticalScores_and_NormaliseValues_Temp(
+                data.vital_data
+              ).then((resolved_data) => {
+                console.log(resolved_data);
+              });
               channel.close();
               connection.close();
             }, seconds * 1000);
@@ -174,10 +179,87 @@ var Calc_CriticalScores_and_NormaliseValues_BP = (data) => {
       dia.push(vital.diastolic_blood_pressure);
       hr.push(vital.pulse);
     });
+    // Finding Mean Values for Vital Data
+    var sys_mean = math.mean(sys);
+    var dia_mean = math.mean(dia);
+    var hr_mean = math.mean(hr);
+
+    // Finding Standard Deviation Values for Vital Data
+    var sys_dev = math.std(sys);
+    var dia_dev = math.std(dia);
+    var hr_dev = math.std(hr);
+
+    // Finding Min and MaxValues for Vital Data
+    var sys_min = math.min(sys);
+    var dia_min = math.min(dia);
+    var hr_min = math.min(hr);
+
+    var sys_max = math.max(sys);
+    var dia_max = math.max(dia);
+    var hr_max = math.max(hr);
+
+    const sys_normal_values = {
+      sys_min: sys_min,
+      sys_max: sys_max,
+      sys_dev: sys_dev,
+      sys_mean: sys_mean,
+    };
+
+    const dia_normal_values = {
+      dia_min: dia_min,
+      dia_max: dia_max,
+      dia_dev: dia_dev,
+      dia_mean: dia_mean,
+    };
+
+    const hr_normal_values = {
+      hr_min: hr_min,
+      hr_max: hr_max,
+      hr_dev: hr_dev,
+      hr_mean: hr_mean,
+    };
+
     const result = {
-      systolic_blood_pressure: sys,
-      diastolic_blood_pressure: dia,
-      Pulse: hr,
+      values: {
+        systolic_blood_pressure: sys,
+        diastolic_blood_pressure: dia,
+        Pulse: hr,
+      },
+      normal_values: {
+        systolic_normal_values: sys_normal_values,
+        diastolic_normal_values: dia_normal_values,
+        pulse_normal_values: hr_normal_values,
+      },
+    };
+    resolve(result);
+  });
+};
+
+var Calc_CriticalScores_and_NormaliseValues_Temp = (data) => {
+  return new Promise(function (resolve, reject) {
+    var temp = [];
+    data.forEach((vital) => {
+      temp.push(vital.temperature);
+    });
+
+    // Finding Min, Max , Mean and Standard Deviation values for Temperature
+    const temp_min = math.min(temp);
+    const temp_max = math.max(temp);
+    const temp_dev = math.dev(temp);
+    const temp_mean = math.mean(temp);
+
+    const temp_normal_values = {
+      temperature_min: temp_min,
+      temperature_max: temp_max,
+      temperature_mean: temp_mean,
+      temperature_dev: temp_dev,
+    };
+
+    const result = {
+      values: {
+        temperature: temp,
+      },
+      normal_values: temp_normal_values,
     };
     resolve(result);
   });
