edf6ce7b8dfeec8750462f0f9a3fd49a2c47e1d2
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2021-12-24 10:23:34 +0530

    IMplemented Critical Scorng API

--- controller/consumerController.js
+++ controller/consumerController.js
@@ -270,43 +270,199 @@ exports.reviewData = async (req, res) => {
 
 exports.calcCriticalScores = async (req, res) => {
   try {
-    const { consume_id } = req.body;
+    const {
+      patientDetails,
+      Sugar_Thyroid_Levels,
+      blood_pressure_data,
+      pulse_data,
+      body_temperature_data,
+    } = req.body;
 
-    const consume_row = await pool.query(
-      "Select * from consumer where consumer_id=$1",
-      [consume_id]
+    const abnormalityScore = criticalAbnormalityScore(
+      patientDetails,
+      Sugar_Thyroid_Levels
+    );
+    const BP_Abnormality = calculateAbnormalityBP(
+      patientDetails,
+      blood_pressure_data
+    );
+    const pulseAbnormality = calculateAbnormalityPulse(
+      patientDetails,
+      pulse_data
+    );
+    const temperatureAbnormality = calculateAbnormalityTemperature(
+      patientDetails,
+      body_temperature_data
     );
 
-    const patient = await pool.query("Select * from patient where id=$1", [
-      consume_row.rows[0].id,
-    ]);
+    var condition = "";
+    if (abnormalityScore >= 5) {
+      condition = "critical condition";
+    } else if (abnormalityScore > 2 && abnormalityScore <= 4) {
+      condition = "moderate condition";
+    } else if (abnormalityScore <= 2) {
+      condition = "mild condition";
+    }
 
-    const manualThyroid_Diabetes_Data = await pool.query(
-      "Select * from thyroid_diabetes where td_id= $1",
-      [consume_row.rows[0].td_id]
-    );
+    const result = {
+      Patient_Condition: condition,
+      Abnormalities: [],
+    };
 
-    const bp_vital_data = await pool.query(
-      "Select * from vital_bp_sensor where vbp_id=$1",
-      [consume_row.rows[0].vbp_id]
-    );
+    BP_Abnormality != null
+      ? result.Abnormalities.push(BP_Abnormality)
+      : console.log("No abnormality in BP found");
+    pulseAbnormality != null
+      ? result.Abnormalities.push(pulseAbnormality)
+      : console.log("No abnormality in Pulse found");
+    temperatureAbnormality != null
+      ? result.Abnormalities.push(temperatureAbnormality)
+      : console.log("No abnormality in Temperature found");
 
-    const temp_vital_data = await pool.query(
-      "Select * from vital_temperature_sensor where vtemp_id=$1",
-      [consume_row.rows[0].vtemp_id]
-    );
-
-    return res.status(200).json({
-      PatientData: patient.rows,
-      Manual_Data: manualThyroid_Diabetes_Data.rows,
-      BP_Vital_Data: bp_vital_data.rows,
-      Temp_Vital_Data: temp_vital_data.rows,
-    });
+    res.status(201).json(result);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
 };
+
+function criticalAbnormalityScore(patientDetails, Sugar_Thyroid_Levels) {
+  const { age, diabetes, prevalentstroke } = patientDetails;
+  const {
+    sugar_post_lunch,
+    sugar_pre_lunch,
+    t3_harmone_value,
+    t4_harmone_value,
+    tsh_value,
+  } = Sugar_Thyroid_Levels;
+
+  var thyroidPresence = false;
+  var thyroidScore = 3;
+  if (parseFloat(tsh_value) > 0.5 && parseFloat(tsh_value) < 4.5)
+    thyroidScore -= 1;
+  if (parseFloat(t3_harmone_value) > 80 && parseFloat(t3_harmone_value) < 220)
+    thyroidScore -= 1;
+  if (
+    parseFloat(t4_harmone_value) >= 5.4 &&
+    parseFloat(t4_harmone_value) <= 11.5
+  )
+    thyroidScore -= 1;
+
+  if (thyroidScore >= 2) thyroidPresence = true;
+
+  var diabetesPresence = true;
+  if (
+    parseFloat(sugar_pre_lunch) >= 70 &&
+    parseFloat(sugar_pre_lunch) <= 100 &&
+    parseFloat(sugar_post_lunch) < 140 &&
+    diabetes != "Yes"
+  ) {
+    diabetesPresence = false;
+  }
+
+  if (
+    parseFloat(sugar_pre_lunch) >= 101 &&
+    parseFloat(sugar_pre_lunch) <= 126 &&
+    parseFloat(sugar_post_lunch) >= 140 &&
+    parseFloat(sugar_post_lunch) < 200 &&
+    diabetes == "Yes"
+  ) {
+    diabetesPresence = false;
+  }
+  age = parseFloat(age);
+
+  var scoring = 0;
+  if (age > 17 && age < 38) {
+    diabetesPresence ? (scoring += 3) : (scoring += 0);
+    thyroidPresence ? (scoring += 2) : (scoring += 0);
+    prevalentstroke ? (scoring += 3) : (scoring += 0);
+  } else if (age > 37 && age < 60) {
+    diabetesPresence ? (scoring += 2) : (scoring += 0);
+    thyroidPresence ? (scoring += 2) : (scoring += 0);
+    prevalentstroke ? (scoring += 2) : (scoring += 0);
+    diabetesPresence && thyroidPresence && prevalentstroke
+      ? (scoring = 8)
+      : (scoring += 0);
+  } else if (age > 59) {
+    diabetesPresence ? (scoring += 1) : (scoring += 0);
+    thyroidPresence ? (scoring += 1) : (scoring += 0);
+    prevalentstroke ? (scoring += 1) : (scoring += 0);
+    diabetesPresence && thyroidPresence && prevalentstroke
+      ? (scoring = 6)
+      : (scoring += 0);
+  }
+  return scoring;
+}
+
+function calculateAbnormalityBP(patientDetails, blood_pressure_data) {
+  const { age } = patientDetails;
+  const { systolic_bp_mean, diastolic_bp_mean } = blood_pressure_data;
+  age = parseInt(age);
+  systolic_bp_mean = parseFloat(systolic_bp_mean);
+  diastolic_bp_mean = parseFloat(diastolic_bp_mean);
+
+  var abnormality = null;
+
+  if (age > 17 && age < 36) {
+    systolic_bp_mean > 150 || diastolic_bp_mean > 100
+      ? (abnormality = "Hypertension")
+      : (abnormality = null);
+    systolic_bp_mean < 82
+      ? (abnormality = "Hypotension")
+      : (abnormality = null);
+  } else if (age > 35 && age < 61) {
+    systolic_bp_mean > 145 || diastolic_bp_mean > 95
+      ? (abnormality = "Hypertension")
+      : (abnormality = null);
+    systolic_bp_mean < 95
+      ? (abnormality = "Hypotension")
+      : (abnormality = null);
+  } else if (age > 60) {
+    systolic_bp_mean > 140 || diastolic_bp_mean > 90
+      ? (abnormality = "Hypertension")
+      : (abnormality = null);
+    systolic_bp_mean < 117
+      ? (abnormality = "Hypotension")
+      : (abnormality = null);
+  }
+}
+
+function calculateAbnormalityPulse(patientDetails, pulse_data) {
+  const { age } = patientDetails;
+  const { pulse_mean } = pulse_data;
+  age = parseInt(age);
+
+  var abnormality = null;
+
+  if (age > 17 && age < 36) {
+    pulse_mean <= 55 ? (abnormality = "Bradycardia") : (abnormality = null);
+    pulse_mean >= 110 ? (abnormality = "Tachycardia") : (abnormality = null);
+  } else if (age > 35 && age < 61) {
+    pulse_mean <= 60 ? (abnormality = "Bradycardia") : (abnormality = null);
+    pulse_mean >= 120 ? (abnormality = "Tachycardia") : (abnormality = null);
+  } else if (age > 60) {
+    pulse_mean <= 65 ? (abnormality = "Bradycardia") : (abnormality = null);
+    pulse_mean >= 100 ? (abnormality = "Tachycardia") : (abnormality = null);
+  }
+}
+
+function calculateAbnormalityTemperature(
+  patientDetails,
+  body_temperature_data
+) {
+  var abnormality = null;
+  const { age } = patientDetails;
+  const { mean_body_temperature } = body_temperature_data;
+  if (parseInt(age) > 17) {
+    parseFloat(mean_body_temperature) < 35.5
+      ? (abnormality = "Hypothermia")
+      : (abnormality = null);
+    parseFloat(mean_body_temperature) > 37.1
+      ? (abnormality = "Fever")
+      : (abnormality = null);
+  }
+  return abnormality;
+}
 
 var Calc_CriticalScores_and_NormaliseValues_BP = async (data, consume_id) => {
   return new Promise(async (resolve, reject) => {
@@ -350,18 +506,18 @@ var Calc_CriticalScores_and_NormaliseValues_BP = async (data, consume_id) => {
       const retured_values = await pool.query(
         "Insert into vital_bp_sensor(systolic_min_bp,systolic_max_bp,systolic_avg_bp, systolic_sd_bp,diastolic_min_bp, diastolic_max_bp, diastolic_avg_bp, diastolic_sd_bp, heartrate_min, heartrate_max,heartrate_avg, heartrate_sd,timestamp) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) returning  vbp_id",
         [
-          parseInt(sys_min),
-          parseInt(sys_max),
-          parseInt(sys_mean),
-          parseInt(sys_dev),
-          parseInt(dia_min),
-          parseInt(dia_max),
-          parseInt(dia_mean),
-          parseInt(dia_dev),
-          parseInt(hr_min),
-          parseInt(hr_max),
-          parseInt(hr_mean),
-          parseInt(hr_dev),
+          parseFloat(sys_min),
+          parseFloat(sys_max),
+          parseFloat(sys_mean),
+          parseFloat(sys_dev),
+          parseFloat(dia_min),
+          parseFloat(dia_max),
+          parseFloat(dia_mean),
+          parseFloat(dia_dev),
+          parseFloat(hr_min),
+          parseFloat(hr_max),
+          parseFloat(hr_mean),
+          parseFloat(hr_dev),
           Date.now(),
         ]
       );
